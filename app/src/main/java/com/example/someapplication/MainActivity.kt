269dd1682b39b6196194efce2b50fe330f484a1e
Roman Lazuko <romanlazuko@gmail.com>
2020-11-15 01:55:13 +0300

    Add MovieDetailsActivity

--- app/src/main/java/com/example/someapplication/MainActivity.kt
+++ app/src/main/java/com/example/someapplication/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.someapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val startDetailsActivityButton = findViewById<Button>(R.id.start_deatails_activity)
+
+        startDetailsActivityButton.setOnClickListener {
+            val intent = Intent(this, MovieDetailsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
