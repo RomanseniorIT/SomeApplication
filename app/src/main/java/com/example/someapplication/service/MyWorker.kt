e71f9f0a77780f710de9cfa6b92baaae503b96c6
Roman Lazuko <romanlazuko@gmail.com>
2021-01-28 22:59:49 +0300

    Add work manager

---
+++ app/src/main/java/com/example/someapplication/service/MyWorker.kt
@@ -0,0 +1,28 @@
+package com.example.someapplication.service
+
+import android.content.Context
+import android.util.Log
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.someapplication.data.MoviesRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.withContext
+
+class MyWorker(context: Context, workerParams: WorkerParameters) :
+    CoroutineWorker(context, workerParams) {
+
+    private val repository = MoviesRepository()
+    override suspend fun doWork(): Result = coroutineScope {
+        try {
+            val result = withContext(Dispatchers.IO) { repository.loadMovies() }
+            if (result.isNullOrEmpty()) {
+                Result.retry()
+            } else {
+                Result.success()
+            }
+        } catch (error: Throwable) {
+            Result.failure()
+        }
+    }
+}
--- app/src/main/java/com/example/someapplication/ui/movies/FragmentMoviesList.kt
+++ app/src/main/java/com/example/someapplication/ui/movies/FragmentMoviesList.kt
@@ -1,19 +1,23 @@
 package com.example.someapplication.ui.movies
 
+import android.annotation.SuppressLint
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.work.*
 import com.example.someapplication.R
 import com.example.someapplication.data.database.movieslist.GenresEntity
 import com.example.someapplication.data.database.movieslist.MoviesListEntity
-import com.example.someapplication.data.model.MoviePreview
+import com.example.someapplication.service.MyWorker
 import com.example.someapplication.ui.moviedetails.FragmentMovieDetails
 import kotlinx.android.synthetic.main.fragment_movie_list.*
+import java.util.concurrent.TimeUnit
 
 class FragmentMoviesList : Fragment(), MoviesListAdapter.Callback {
 
@@ -32,6 +36,7 @@ class FragmentMoviesList : Fragment(), MoviesListAdapter.Callback {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         initObservers()
+        initWorker()
         viewModel.getCachedGenres()
     }
 
@@ -56,11 +61,32 @@ class FragmentMoviesList : Fragment(), MoviesListAdapter.Callback {
         })
     }
 
+    @SuppressLint("RestrictedApi")
+    private fun initWorker() {
+        val constr = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresCharging(true)
+            .build()
+
+        val uploadWork =
+            PeriodicWorkRequest.Builder(MyWorker::class.java, 8, TimeUnit.HOURS)
+                .setConstraints(constr)
+                .addTag("MyWorker")
+                .build()
+
+        val workManager = WorkManager.getInstance(requireActivity()).enqueue(uploadWork)
+        workManager.state.observe(viewLifecycleOwner, { state ->
+            if (state == Operation.SUCCESS) {
+                viewModel.getCachedGenres()
+            }
+        })
+    }
+
     override fun startMovieDetailsFragment(item: MoviesListEntity) {
-        fragmentManager
-            ?.beginTransaction()
-            ?.replace(R.id.fragment_container, FragmentMovieDetails.newInstance(item.id))
-            ?.addToBackStack(null)
-            ?.commit()
+        childFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, FragmentMovieDetails.newInstance(item.id))
+            .addToBackStack(null)
+            .commit()
     }
 }
